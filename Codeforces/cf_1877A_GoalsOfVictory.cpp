3007776a38c3d76ec3278d6e9c802a6ccb07b860
Abdur Rahman Sahil <sahilabdurrahman96@gmail.com>
2026-08-28 19:12:55 +0600

    Solved CF_1877A_GoalsOfVictory

---
+++ Codeforces/cf_1877A_GoalsOfVictory.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> a;
+        for(int i = 0; i < n-1; i++){
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+
+        int sum = 0;
+        for(int val : a){
+            sum += val;
+        }
+
+        cout << -(sum) << '\n';
+    }
+
+    return 0;
+}
