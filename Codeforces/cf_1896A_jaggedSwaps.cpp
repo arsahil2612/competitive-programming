6e9963bf4093071e202a38e46897565c2b7e1b50
Abdur Rahman Sahil <sahilabdurrahman96@gmail.com>
2026-08-31 00:16:44 +0600

    Solved CF_1896A_jaggedSwaps

---
+++ Codeforces/cf_1896A_jaggedSwaps.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for(int i = 1; i <= n; i++)
+        {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+
+        if(a[0] == 1){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
